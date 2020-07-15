505624cad5d8e10fdeb3899dbf8b072fcb45ecad
mo <245994626@qq.com>
2020-07-15 18:11:15 +0800

    编写信息类。

---
+++ app/src/main/java/com/example/mnistclassifier/Message.java
@@ -0,0 +1,44 @@
+package com.example.mnistclassifier;
+
+
+import android.graphics.Bitmap;
+
+public class Message {
+    public static final int TYPE_RECEIVE = 0;
+    public static final int TYPE_SEND = 1;
+    public static final int TYPE_TEXT = 2;
+    public static final int TYPE_IMG = 3;
+    private String text_content;
+    private Bitmap img_content;
+
+    private int type;
+    private int content_type;
+
+    public Message(String content, int type) {
+        this.text_content = content;
+        this.type = type;
+        this.content_type=Message.TYPE_TEXT;
+    }
+
+    public Message(Bitmap content, int type) {
+        this.img_content = content;
+        this.type = type;
+        this.content_type=Message.TYPE_IMG;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public int getContent_type() {
+        return content_type;
+    }
+
+    public String getText_content() {
+        return text_content;
+    }
+
+    public Bitmap getImg_content() {
+        return img_content;
+    }
+}
