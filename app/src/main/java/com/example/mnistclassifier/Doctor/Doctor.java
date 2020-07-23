5e15de2565afad5223b2e6094f39c1e2d2f382e8
mo <245994626@qq.com>
2020-07-23 15:38:26 +0800

    基于新建的节点类与边类，重构医生类。

--- app/src/main/java/com/example/mnistclassifier/Doctor/Doctor.java
+++ app/src/main/java/com/example/mnistclassifier/Doctor/Doctor.java
@@ -1,157 +1,111 @@
-package com.example.mnistclassifier;
+package com.example.mnistclassifier.Doctor;
 
-import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
 
-public class Doctor {
-    private enum Status {
-        ASK_POSITION,
-        POSITION_TOP,
-        ASK_TOP,
-        POSITION_BASE,
-        ASK_FALL;
+public class Doctor {//医生类
+
+    private Vertex current_vertex;//医生当前所处流程节点处
+
+    private Vertex ask_pos;//询问叶片所处位置节点
+    private Edge ask_pos_to_top;//叶片位于顶梢
+    private Edge ask_pos_to_base;//叶片位于基枝
+
+    private Vertex ask_top;//初选基枝，询问顶梢是否异常
+    private Edge top_abnormal;//初选基枝，且顶梢异常
+    private Edge top_normal;//初选基枝，且顶梢无异常
+
+    private Vertex top_cls;//顶梢图片识别节点
+    private Edge top_healthy;//顶梢健康
+    private Edge top_avg;//顶梢均匀型黄化
+    private Edge top_spot;//顶梢斑驳型黄化
+    private Edge top_zn;//顶梢缺锌
+    private Edge top_mn;//顶梢缺锰
+    private Edge top_mg;//顶梢缺镁
+    private Edge top_unknown;//顶梢不明症状
+
+    private Vertex base_cls;//基枝图片识别节点
+    private Edge base_healthy;//基枝健康
+    private Edge base_avg;//基枝均匀型黄化
+    private Edge base_spot;//基枝斑驳型黄化
+    private Edge base_zn;//基枝缺锌
+    private Edge base_mn;//基枝缺锰
+    private Edge base_mg;//基枝缺镁
+    private Edge base_unknown;//基枝不明症状
+
+    private Vertex ask_fall;//顶梢缺素情况下，询问是否有大量落叶
+    private Edge has_fall;//有大量落叶
+    private Edge no_fall;//没有落叶
+
+
+    public Doctor() {
+        //初始化节点
+        ask_pos = new Vertex("请回答您所要判断的叶片所在位置（顶梢/基枝）");
+        ask_top = new Vertex("请准确回复顶梢是否异常。（是/否）");
+        top_cls = new Vertex("顶梢识别过程有误或未上传顶梢叶片图片。");
+        base_cls = new Vertex("基枝识别过程有误或未上传基枝叶片图片。。");
+        ask_fall = new Vertex("请准确回答柑橘是否有大量落叶？（是/否）");
+
+        //初始化边
+        ask_pos_to_top = new Edge(top_cls, "顶梢", "请上传顶梢叶片的图片。");
+        ask_pos_to_base = new Edge(ask_top, "基枝", "请问顶梢是否异常？");
+
+        top_abnormal = new Edge(top_cls, "是", "请上传顶梢叶片的图片。");
+        top_normal = new Edge(base_cls, "否", "请上传基枝叶片的图片");
+
+        top_healthy = new Edge(ask_pos, "0", "您的顶梢叶片非常健康。");
+        top_avg = new Edge(ask_pos, "1", "您的顶梢叶片呈均匀型黄化，属黄龙病症状。");
+        top_spot = new Edge(ask_pos, "2", "您的顶梢叶片呈斑驳型黄化，属黄龙病症状。");
+        top_zn = new Edge(ask_fall, "3", "您的顶梢叶片有缺锌症状。请问柑橘是否有大量落叶？");
+        top_mn = new Edge(ask_fall, "4", "您的顶梢叶片有缺锰症状。请问柑橘是否有大量落叶？");
+        top_mg = new Edge(ask_fall, "5", "您的顶梢叶片有缺镁症状。请问柑橘是否有大量落叶？");
+        top_unknown = new Edge(ask_pos, "other", "您的顶梢叶片症状原因不明。");
+
+        base_healthy = new Edge(ask_pos, "0", "您的基枝叶片非常健康。");
+        base_avg = new Edge(ask_pos, "1", "您的基枝叶片呈均匀型黄化，但患黄龙病的可能性较低。");
+        base_spot = new Edge(ask_pos, "2", "您的基枝叶片呈斑驳型黄化，但患黄龙病的可能性较低。");
+        base_zn = new Edge(ask_pos, "3", "您的基枝叶片有缺锌症状。属生理性缺素症。");
+        base_mn = new Edge(ask_pos, "4", "您的基枝叶片有缺锰症状。属生理性缺素症。");
+        base_mg = new Edge(ask_pos, "5", "您的基枝叶片有缺镁症状。属生理性缺素症。");
+        base_unknown = new Edge(ask_pos, "other", "您的基枝叶片症状原因不明。");
+
+        has_fall = new Edge(ask_pos, "是", "您的柑橘可能患黄龙病，且病情处于中后期。");
+        no_fall = new Edge(ask_pos, "否", "您的柑橘有生理性缺素症。");
+
+        //将边加入到节点当中
+        ask_pos.setEdgeList(Arrays.asList(ask_pos_to_top, ask_pos_to_base));
+        ask_top.setEdgeList(Arrays.asList(top_abnormal, top_normal));
+        top_cls.setEdgeList(Arrays.asList(
+                top_healthy,
+                top_avg,
+                top_spot,
+                top_zn,
+                top_mn,
+                top_mg,
+                top_unknown
+        ));
+        base_cls.setEdgeList(Arrays.asList(
+                base_healthy,
+                base_avg,
+                base_spot,
+                base_zn,
+                base_mn,
+                base_mg,
+                base_unknown
+        ));
+        ask_fall.setEdgeList(Arrays.asList(has_fall, no_fall));
+
+        //设置初始节点
+        current_vertex = ask_pos;
     }
 
-    private Status status;
-    private Classifier classifier;
-
-    private MsgManager msgManager;
-
-    public Doctor(MainActivity mainActivity) {
-        this.msgManager = mainActivity.getMsgManager();
-        status = Status.ASK_POSITION;
-        try {
-            classifier = new Classifier(mainActivity);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        msgManager.addTextMsg("欢迎使用柑橘叶片疾病诊断系统。", Msg.TYPE_RECEIVE); // 信息列表里添加欢迎语
-        msgManager.addTextMsg("请问您要诊断的叶片位于顶梢还是基枝？", Msg.TYPE_RECEIVE);
+    public boolean waitForImg() {//判断医生当前是否处于等待图片信息的状态
+        return top_cls == current_vertex || base_cls == current_vertex;
     }
 
-    public void diagnose() {//对用户提供的信息进行诊断
-        Msg msg = msgManager.getLastMsg();//获取最新的信息
-        if (Msg.TYPE_RECEIVE == msg.getType()) return;//最新信息为医生发送的内容
-        switch (msg.getContent_type()) {//按信息类型，分情况分析
-            case Msg.TYPE_TEXT://文本信息
-                switch (status) {
-                    case ASK_POSITION://系统询问异常叶片所处位置
-                        switch (msg.getText_content()) {
-                            case "顶梢"://用户回答，叶片位于顶梢
-                                msgManager.addTextMsg("请上传顶梢叶片的图片。", Msg.TYPE_RECEIVE);
-                                status = Status.POSITION_TOP;//等待识别用户提交顶梢叶片图片
-                                break;
-                            case "基枝"://用户回答，叶片位于基枝
-                                msgManager.addTextMsg("请问顶梢部位的叶片是否有异常情况？（是/否）", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_TOP;//等待用户回答顶梢部位的情况
-                                break;
-                            default://用户没有正确回答异常叶片所处位置
-                                msgManager.addTextMsg("请问您要诊断的叶片位于顶梢还是基枝？（顶梢/基枝）", Msg.TYPE_RECEIVE);
-                        }
-                        break;
-                    case POSITION_TOP://用户在系统等待接收图片时，依旧发送文字信息，此时应提醒用户上传图片
-                        msgManager.addTextMsg("请上传顶梢叶片的图片。", Msg.TYPE_RECEIVE);
-                        break;
-                    case ASK_TOP://系统询问用户顶梢部位是否异常
-                        switch (msg.getText_content()) {
-                            case "是"://用户在先表示基枝异常的情况下，又表示顶梢有异常，则优先诊断顶梢叶片
-                                msgManager.addTextMsg("请上传顶梢叶片的图片。", Msg.TYPE_RECEIVE);
-                                status = Status.POSITION_TOP;//等待识别用户提交顶梢叶片图片
-                                break;
-                            case "否"://用户表示基枝异常，顶梢正常，则诊断基枝叶片
-                                msgManager.addTextMsg("请上传基枝叶片的图片。", Msg.TYPE_RECEIVE);
-                                status = Status.POSITION_BASE;//等待识别用户提交基枝叶片图片
-                                break;
-                            default://用户没有正确回答顶梢部位的情况
-                                msgManager.addTextMsg("请准确回答顶梢是否有异常。（是/否）", Msg.TYPE_RECEIVE);
-                        }
-                        break;
-                    case ASK_FALL://系统询问柑橘是否有大量落叶
-                        switch (msg.getText_content()) {
-                            case "是":
-                                msgManager.addTextMsg("您的柑橘可能患黄龙病，病情处于中后期。", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_POSITION;
-                                break;
-                            case "否":
-                                msgManager.addTextMsg("您的柑橘可能有生理性缺素。", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_POSITION;
-                                break;
-                            default://用户没有正确回答落叶的情况
-                                msgManager.addTextMsg("请准确回答柑橘是否有大量落叶。（是/否）", Msg.TYPE_RECEIVE);
-                        }
-                        break;
-                }
-                break;
-            case Msg.TYPE_IMG:
-                // 对图片进行分类
-                int classify_res = classifier.classify(msg.getImg_content());
-                switch (status) {//根据叶片来自部位的不同，分情况分析
-                    case POSITION_TOP://顶梢
-                        switch (classify_res) {
-                            case 0:
-                                msgManager.addTextMsg("您的顶梢叶片非常健康。", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_POSITION;//诊断结束，节点状态回到原始
-                                break;
-                            case 1:
-                                msgManager.addTextMsg("您的顶梢叶片呈均匀型黄化特征，您的柑橘可能患黄龙病。", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_POSITION;//诊断结束，节点状态回到原始
-                                break;
-                            case 2:
-                                msgManager.addTextMsg("您的顶梢叶片呈斑驳型黄化特征，您的柑橘可能患黄龙病。", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_POSITION;//诊断结束，节点状态回到原始
-                                break;
-                            case 3:
-                                msgManager.addTextMsg("您的顶梢叶片有缺锌症状。请问柑橘是否有大量落叶？", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_FALL;//切换至询问落叶状态
-                                break;
-                            case 4:
-                                msgManager.addTextMsg("您的顶梢叶片有缺锰症状。请问柑橘是否有大量落叶？", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_FALL;//切换至询问落叶状态
-                                break;
-                            case 5:
-                                msgManager.addTextMsg("您的顶梢叶片有缺镁症状。请问柑橘是否有大量落叶？", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_FALL;//切换至询问落叶状态
-                                break;
-                            default:
-                                msgManager.addTextMsg("您的顶梢叶片有一定程度的黄化，但病因非黄龙病或生理性缺素。", Msg.TYPE_RECEIVE);
-                                status = Status.ASK_POSITION;//诊断结束，节点状态回到原始
-                        }
-                        break;
-                    case POSITION_BASE://基枝
-                        switch (classify_res) {
-                            case 0:
-                                msgManager.addTextMsg("您的基枝叶片非常健康。", Msg.TYPE_RECEIVE);
-                                break;
-                            case 3:
-                                msgManager.addTextMsg("您的柑橘可能生理性缺锌。", Msg.TYPE_RECEIVE);
-                                break;
-                            case 4:
-                                msgManager.addTextMsg("您的柑橘可能生理性缺锰。", Msg.TYPE_RECEIVE);
-                                break;
-                            case 5:
-                                msgManager.addTextMsg("您的柑橘可能生理性缺镁。", Msg.TYPE_RECEIVE);
-                                break;
-                            default:
-                                msgManager.addTextMsg("您的基枝叶片有一定程度的黄化，但病因非黄龙病或生理性缺素。", Msg.TYPE_RECEIVE);
-                        }
-                        status = Status.ASK_POSITION;//诊断结束，节点状态回到原始
-                        break;
-                }
-                break;
-
-        }
-
-    }
-
-    public boolean waitForImg() {
-        return status == Status.POSITION_TOP || status == Status.POSITION_BASE;
-    }
-
-    private static class Vertex{
-        private List<Edge> outEdges;
-    }
-
-    private static class Edge{
-        
+    public String response(String content) {//医生根据用户的描述内容作相应答复
+        Edge edge = current_vertex.selectEdge(content);//从当前节点，根据用户内容，找到相应去向
+        if (null == edge) return current_vertex.getResponse();//当前节点没有一个去向与用户描述内容吻合
+        current_vertex = edge.getVertex();//找到相应情况，将当前节点切换到新的节点
+        return edge.getResponse();//返回该去向的对应答复
     }
 }
